dd2409be06cbdf5f51b84606277573ad44678f87
Nathan <Naathanferreira26@gmail.com>
2020-07-28 18:16:50 -0300

    Exercicios complementares

---
+++ ExerciciosComplementares/src/br/com/rd/rdevs/javalang/JavaLang.java
@@ -0,0 +1,92 @@
+package br.com.rd.rdevs.javalang;
+
+public class JavaLang {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 Exercicio 1 
+			
+		 
+		String frase = "Nathanael Ferreira da Silva";
+		if (frase.contains("Ferreira")){
+			System.out.println("Contém"); 
+			 }else{ System.out.println("Não contém"); 
+		}
+
+	}
+ 	
+		String strOrigem = "     Nathanael      Ferreira     da       Silva     ";  
+		strOrigem = strOrigem.replaceAll("\\s+"," ");
+		System.out.println(strOrigem);
+		
+		
+	}
+}
+
+
+
+
+
+String verifica = "";
+
+        if (verifica.trim().equals("")) {
+            System.out.println("String Vazia");
+        } else {
+            System.out.println("String Preenchida");
+            }
+	}
+}
+
+	
+ String a = "Nathanael";
+ System.out.println("A String que você criou tem " + a.length() + " letras");
+
+}}
+*/
+		
+		
+	/*
+	 Exercicio 2	
+	 
+		String nome = "Nathanael";
+		for (int i = nome.length()-1; i >= 0; i--){ System.out.println(nome.charAt(i)); 
+		}
+		}
+}
+		*/
+		
+		
+		/*
+		 
+		 Exercicio 3
+
+		String palavra = "socorram me, subi no ônibus em marrocos";
+			
+			        inverteComStringBuilder(palavra);
+			
+			    }
+			
+			     public static void inverteComStringBuilder(String texto) {
+			         System.out.print("\t");
+			         StringBuilder invertido = new StringBuilder(texto).reverse();
+			         System.out.println(invertido);
+			     }
+			   }
+			*/
+		
+		/*
+		 Exercicio 4
+		 
+		int[] arrayInteiros = new int[3];
+		char[] arrayChar = new char[3];
+		int inteiro = arrayInteiros[3];
+		
+		String c = "762";
+		for (int i=0; i<3; i++){
+			arrayChar[i] = c.charAt(i);
+			arrayInteiros[i] = arrayChar[i];
+		}
+		System.out.println(inteiro);
+	}
+} */
